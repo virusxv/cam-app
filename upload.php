a8e25dfc328680ad7c43adcb20d194a323f2a5ce
virusxv <98118756+virusxv@users.noreply.github.com>
2023-05-15 12:06:39 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,1 +1,8 @@
 
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['photo'])) {
+  $photo = base64_decode($_POST['photo']);
+  file_put_contents('/ścieżka/do/katalogu/na/serwerze/zdjęcia.jpg', $photo);
+  echo 'Zdjęcie zostało zapisane na serwerze.';
+}
+?>
